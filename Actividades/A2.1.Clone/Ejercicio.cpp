3fe3d8aa6eb1d63d8564fb65b22d92cdedcc18b4
CesarValladares <cesar_valla@hotmail.com>
2018-01-23 14:15:08 -0600

    23/01

--- Actividades/A2.1.Clone/Ejercicio.cpp
+++ Actividades/A2.1.Clone/Ejercicio.cpp
@@ -53,7 +53,7 @@ public:
 
     Tablet(string branch) : branch(new string(branch))
     {
-        cout << "Branch " << branch << endl;
+
     }
     
 };
@@ -62,26 +62,66 @@ class Smartphone : public Moviles
 {
 public:
     int* phone_number; 
+
+    Smartphone (const Moviles& temp)
+    {
+        phone_number = new int(*(this->phone_number));
+        serial_number = new int(*(temp.serial_number));
+        ID = new int (*(temp.ID));
+        type = (temp.type);
+        usuario = (temp.usuario);
+    }
+
     Moviles* clone()
     {
         return new Smartphone(*this);
     }
     
-};
+    Smartphone(int phone_number) : phone_number(new int(phone_number))
+    {
+
+    }
+
+}; 
+
 class Smartwatch : public Moviles
 {
 public:
     int* hour;
+
+    Smartwatch (const Moviles& temp)
+    {
+        hour = new int(*(this->hour));
+        serial_number = new int(*(temp.serial_number));
+        ID = new int (*(temp.ID));
+        type = (temp.type);
+        usuario = (temp.usuario);
+    }
+
     Moviles* clone()
     {
         return new Smartwatch(*this);
     }
     
+    Smartwatch(int hour) : hour(new int(hour))
+    {
+
+    }
+
 };
 
 int main()
 {
     Tablet* T1 = new Tablet("Apple"); 
-    T1->encender(); 
+    cout << "ANTES"<< endl;
+    cout << endl;
+    cout << "Tablet 1: "<< *T1->branch << endl;
+    Tablet T2 = *T1;
+    cout << "Tablet 2: "<< *T2.branch << endl;
+    cout << endl;
+    cout << "DESPUES" << endl;
+    cout << "Tablet 1: "<< *T1->branch << endl;
+    *T2.branch = "Samsung";
+    cout << "Tablet 2: "<< *T2.branch << endl;
     return 0; 
 }
